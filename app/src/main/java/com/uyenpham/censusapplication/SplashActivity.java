c0ecbcc85599afe9d5b5fdb786d03dc602361369
uyenpt <uyenpt@steadfast-inc.com>
2018-09-12 23:50:24 +0700

    start

---
+++ app/src/main/java/com/uyenpham/censusapplication/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.uyenpham.censusapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
